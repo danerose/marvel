87057fecf9a4d778b66b97ab113f73ebc064178a
Danerose <valencia3698@gmail.com>
2023-06-11 19:33:31 -0500

    Character Detail state added and inject repository

--- lib/app/view_model/details/character_detail_bloc.dart
+++ lib/app/view_model/details/character_detail_bloc.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:marvel/app/view_model/characters/character_event.dart';
+
+import 'package:marvel/app/repositories/interfaces/marvel_repository.dart';
+import 'package:marvel/app/view_model/details/character_detail_event.dart';
 import 'package:marvel/app/view_model/details/character_detail_state.dart';
 
-class CharacterDetailBloc extends Bloc<CharacterEvent, CharacterDetailState> {
-  CharacterDetailBloc() : super(const CharacterDetailState()) {}
+class CharacterDetailBloc extends Bloc<CharDetailEvent, CharDetailState> {
+  final MarvelRepository _marvelRepository;
+  CharacterDetailBloc(
+    this._marvelRepository,
+  ) : super(const CharDetailState()) {
+    on<CharacteDetailLoad>(_onLoadDetails);
+  }
+
+  Future<void> _onLoadDetails(
+    CharacteDetailLoad event,
+    Emitter<CharDetailState> emit,
+  ) async {}
 }
--- lib/app/view_model/details/character_detail_event.dart
+++ lib/app/view_model/details/character_detail_event.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:equatable/equatable.dart';
+import 'package:marvel/app/model/character/character_model.dart';
 
 @immutable
-abstract class CharacterDetailEvent extends Equatable {
+abstract class CharDetailEvent extends Equatable {
   @override
   List<Object?> get props => [];
+}
+
+class CharacteDetailLoad extends CharDetailEvent {
+  CharacteDetailLoad({required this.character});
+  final Character character;
 }
